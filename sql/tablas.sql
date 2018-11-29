b43a0cf4f5d5d8e6d17ff2cbe86673a81a23dc1e
ErikEllison <ellison@ErikEllisons-MacBook-Pro.local>
2018-11-28 18:06:23 -0600

    Script agregados

--- sql/tablas.sql
+++ sql/tablas.sql
@@ -1,48 +1,56 @@
 CREATE TABLE estado (
   clave INT(11) NOT NULL PRIMARY KEY,
-  descripcion VARCHAR (60)
+  descripcion VARCHAR (60) NOT NULL
 );
 CREATE TABLE municipio (
   clave INT(11) NOT NULL PRIMARY KEY,
-  descripcion VARCHAR (60),
+  descripcion VARCHAR (60) NOT NULL,
   id_estado INT(11) NOT NULL REFERENCES estado (clave)
 );
-CREATE TABLE localidad (
-  clave INT(11) NOT NULL PRIMARY KEY,
-  descripcion VARCHAR (60),
-  id_municipio INT(11) NOT NULL REFERENCES municipio (clave)
-);
+
 
 CREATE TABLE familia_linguistica (
   id INT(11) NOT NULL PRIMARY KEY,
-  descripcion VARCHAR (60)
+  descripcion VARCHAR (60) NOT NULL
 );
-
-CREATE TABLE variante_linguistica (
-  id INT(11) NOT NULL PRIMARY KEY,
-  descripcion VARCHAR (60)
-);
-
 CREATE TABLE agrupacion_linguistica (
   id INT(11) NOT NULL PRIMARY KEY,
-  descripcion VARCHAR (60),
-  id_localidad INT(11) NOT NULL REFERENCES localidad (clave),
-  id_familia_linguistica INT(11) NOT NULL REFERENCES familia_linguistica (id),
-  id__variante_linguistica INT(11) NOT NULL REFERENCES variante_linguistica (id)
+  descripcion VARCHAR (60) NOT NULL,
+  id_familia_linguistica INT(11) NOT NULL REFERENCES familia_linguistica (id)
 );
+CREATE TABLE variante_linguistica (
+  id INT(11) NOT NULL PRIMARY KEY,
+  descripcion VARCHAR (60),
+  id_agrupacion_linguistica INT(11) NOT NULL REFERENCES agrupacion_linguistica (id)
+);
+
+
+CREATE TABLE localidad (
+  clave INT(11) NOT NULL PRIMARY KEY,
+  descripcion VARCHAR (60) NOT NULL,
+  id_municipio INT(11) NOT NULL REFERENCES municipio (clave),
+  id_variante_linguistica INT(11) NOT NULL REFERENCES variante_linguistica (id)
+);
+
 
 CREATE TABLE tipo (
   id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  titulo VARCHAR (60),
-  tipo VARCHAR (60),
-  clasificacion VARCHAR (60)
+  clasificacion VARCHAR (60) NOT NULL
 );
-
+CREATE TABLE historias(
+  id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  titulo VARCHAR (60),
+  escritura TEXT,
+  pronunciacion VARCHAR (60),
+  significado TEXT NOT NULL, 
+  id_localidad INT(11) NOT NULL REFERENCES localidad (id),
+  id_tipo INT(11) NOT NULL REFERENCES tipo (id)  
+);
 CREATE TABLE palabras (
   id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   escritura VARCHAR (60),
-  pronunciacion VARCHAR (60), 
-  significado VARCHAR (60), 
+  pronunciacion VARCHAR (60),
+  significado VARCHAR (60) NOT NULL, 
   id_localidad INT(11) NOT NULL REFERENCES localidad (id),
   id_tipo INT(11) NOT NULL REFERENCES tipo (id)  
 );
