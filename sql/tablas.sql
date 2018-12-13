06aa579421b36ab334bd8edf33f84669648ef550
SantiagoJoseAntonio <43587286+SantiagoJoseAntonio@users.noreply.github.com>
2018-12-12 23:02:24 -0600

    Update tablas.sql

--- sql/tablas.sql
+++ sql/tablas.sql
@@ -1,12 +1,11 @@
 
-
-
-
 -- Volcando estructura de base de datos para lenguas
+DROP DATABASE IF EXISTS `lenguas`;
 CREATE DATABASE IF NOT EXISTS `lenguas` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `lenguas`;
 
 -- Volcando estructura para tabla lenguas.agrupacion_linguistica
+DROP TABLE IF EXISTS `agrupacion_linguistica`;
 CREATE TABLE IF NOT EXISTS `agrupacion_linguistica` (
   `id` int(11) NOT NULL,
   `descripcion` varchar(60) NOT NULL,
@@ -20,6 +19,7 @@ DELETE FROM `agrupacion_linguistica`;
 /*!40000 ALTER TABLE `agrupacion_linguistica` ENABLE KEYS */;
 
 -- Volcando estructura para tabla lenguas.clasificacion
+DROP TABLE IF EXISTS `clasificacion`;
 CREATE TABLE IF NOT EXISTS `clasificacion` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(60) NOT NULL,
@@ -32,6 +32,7 @@ DELETE FROM `clasificacion`;
 /*!40000 ALTER TABLE `clasificacion` ENABLE KEYS */;
 
 -- Volcando estructura para tabla lenguas.estado
+DROP TABLE IF EXISTS `estado`;
 CREATE TABLE IF NOT EXISTS `estado` (
   `clave` int(11) NOT NULL,
   `descripcion` varchar(60) NOT NULL,
@@ -44,6 +45,7 @@ DELETE FROM `estado`;
 /*!40000 ALTER TABLE `estado` ENABLE KEYS */;
 
 -- Volcando estructura para tabla lenguas.familia_linguistica
+DROP TABLE IF EXISTS `familia_linguistica`;
 CREATE TABLE IF NOT EXISTS `familia_linguistica` (
   `id` int(11) NOT NULL,
   `descripcion` varchar(60) NOT NULL,
@@ -56,6 +58,7 @@ DELETE FROM `familia_linguistica`;
 /*!40000 ALTER TABLE `familia_linguistica` ENABLE KEYS */;
 
 -- Volcando estructura para tabla lenguas.localidad
+DROP TABLE IF EXISTS `localidad`;
 CREATE TABLE IF NOT EXISTS `localidad` (
   `clave` int(11) NOT NULL,
   `descripcion` varchar(60) NOT NULL,
@@ -70,6 +73,7 @@ DELETE FROM `localidad`;
 /*!40000 ALTER TABLE `localidad` ENABLE KEYS */;
 
 -- Volcando estructura para tabla lenguas.municipio
+DROP TABLE IF EXISTS `municipio`;
 CREATE TABLE IF NOT EXISTS `municipio` (
   `clave` int(11) NOT NULL,
   `descripcion` varchar(60) NOT NULL,
@@ -83,6 +87,7 @@ DELETE FROM `municipio`;
 /*!40000 ALTER TABLE `municipio` ENABLE KEYS */;
 
 -- Volcando estructura para tabla lenguas.palabras
+DROP TABLE IF EXISTS `palabras`;
 CREATE TABLE IF NOT EXISTS `palabras` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `escritura` varchar(60) DEFAULT NULL,
@@ -100,6 +105,7 @@ DELETE FROM `palabras`;
 /*!40000 ALTER TABLE `palabras` ENABLE KEYS */;
 
 -- Volcando estructura para tabla lenguas.significado
+DROP TABLE IF EXISTS `significado`;
 CREATE TABLE IF NOT EXISTS `significado` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `escritura` varchar(60) DEFAULT NULL,
@@ -112,6 +118,7 @@ DELETE FROM `significado`;
 /*!40000 ALTER TABLE `significado` ENABLE KEYS */;
 
 -- Volcando estructura para tabla lenguas.variante_linguistica
+DROP TABLE IF EXISTS `variante_linguistica`;
 CREATE TABLE IF NOT EXISTS `variante_linguistica` (
   `id` int(11) NOT NULL,
   `descripcion` varchar(60) DEFAULT NULL,
@@ -121,6 +128,7 @@ CREATE TABLE IF NOT EXISTS `variante_linguistica` (
 
 -- Volcando datos para la tabla lenguas.variante_linguistica: ~0 rows (aproximadamente)
 DELETE FROM `variante_linguistica`;
+
 
 
 
